1ac365e522cc711647fdc85c7747f9e44979bd2e
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2022-10-02 06:12:39 -0400

    web3 moment

---
+++ frontend/src/hooks/useWeb3Modal.ts
@@ -0,0 +1,79 @@
+import { Web3Provider } from "@ethersproject/providers";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Web3Modal from "web3modal";
+
+// Enter a valid infura key here to avoid being rate limited
+// You can get a key for free at https://infura.io/register
+const INFURA_ID = process.env.REACT_APP_INFURA_KEY;
+
+const NETWORK = "mainnet";
+
+type CONFIG_TYPE = {
+	autoLoad?: boolean;
+	infuraId?: string;
+	network?: string;
+};
+
+function useWeb3Modal(config = {}) {
+	const [provider, setProvider] = useState<Web3Provider>();
+	const [autoLoaded, setAutoLoaded] = useState(false);
+	const {
+		autoLoad = true,
+		infuraId = INFURA_ID,
+		network = NETWORK,
+	}: CONFIG_TYPE = config as CONFIG_TYPE;
+
+	// console.log('infura', INFURA_ID);
+
+	// Web3Modal also supports many other wallets.
+	// You can see other options at https://github.com/Web3Modal/web3modal
+	const web3Modal = useMemo(() => {
+		return new Web3Modal({
+			network,
+			cacheProvider: true,
+			providerOptions: {
+				walletconnect: {
+					package: WalletConnectProvider,
+					options: {
+						rpc: {
+							1: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+						},
+					},
+				},
+			},
+		});
+	}, [infuraId, network]);
+
+	// Open wallet selection modal.
+	const loadWeb3Modal = useCallback(async () => {
+		const newProvider = await web3Modal.connect();
+		setProvider(new Web3Provider(newProvider));
+	}, [web3Modal]);
+
+	const logoutOfWeb3Modal = useCallback(
+		async function () {
+			await web3Modal.clearCachedProvider();
+			window.location.reload();
+		},
+		[web3Modal]
+	);
+
+	// If autoLoad is enabled and the the wallet had been loaded before, load it automatically now.
+	useEffect(() => {
+		if (autoLoad && !autoLoaded && web3Modal.cachedProvider) {
+			loadWeb3Modal();
+			setAutoLoaded(true);
+		}
+	}, [
+		autoLoad,
+		autoLoaded,
+		loadWeb3Modal,
+		setAutoLoaded,
+		web3Modal.cachedProvider,
+	]);
+
+	return [provider, loadWeb3Modal, logoutOfWeb3Modal];
+}
+
+export default useWeb3Modal;
